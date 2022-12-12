613fe9e0634c6a78c5af0f31d9ef2321697c2ada
Jonathan Calderon <jaksvafa7@gmail.com>
2022-12-12 13:54:27 +0100

    I love Django for now

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Article)
-admin.site.register(models.Reporter)
+# admin.site.register(models.Article)
+# admin.site.register(models.Reporter)
+
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-# Create your models here.
-class Reporter(models.Model):
-    full_name = models.CharField(max_length=70)
 
-class Article(models.Model):
-    pub_date = models.DateField()
-    headline = models.CharField(max_length=200)
-    content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
