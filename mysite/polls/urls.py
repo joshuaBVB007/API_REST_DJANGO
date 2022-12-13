2abedf935241365f02f56909f8eb2485c0c46043
Jonathan Calderon <jaksvafa7@gmail.com>
2022-12-13 17:12:37 +0100

    generics views added

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,13 +8,21 @@ from . import views
 # Namespace for this app
 app_name='polls'
 
+
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:id>/vote/', views.vote, name='vote'),
 ]
+
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:id>/vote/', views.vote, name='vote'),
+# ]
